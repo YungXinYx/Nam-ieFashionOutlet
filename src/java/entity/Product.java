41943bd9257809dc0f03a69467ef9e68d10e27a6
HO HONG MENG <88657028+hohongmeng@users.noreply.github.com>
2022-03-08 22:48:58 +0800

    Adding Entity and Register form in html + css
    All tables for entities have been created
    
    Register form in html + css is added

---
+++ src/java/entity/Product.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author melvi
+ */
+@Entity
+@Table(name = "PRODUCT")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Product.findAll", query = "SELECT p FROM Product p")
+    , @NamedQuery(name = "Product.findByProductid", query = "SELECT p FROM Product p WHERE p.productid = :productid")
+    , @NamedQuery(name = "Product.findByProductname", query = "SELECT p FROM Product p WHERE p.productname = :productname")
+    , @NamedQuery(name = "Product.findByProductprice", query = "SELECT p FROM Product p WHERE p.productprice = :productprice")})
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 15)
+    @Column(name = "PRODUCTID")
+    private String productid;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 30)
+    @Column(name = "PRODUCTNAME")
+    private String productname;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "PRODUCTPRICE")
+    private Float productprice;
+    @JoinColumn(name = "CATEGORYID", referencedColumnName = "CATEGORYID")
+    @ManyToOne(optional = false)
+    private Category categoryid;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "productid")
+    private List<ProductDetails> productDetailsList;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "productid")
+    private List<Feedback> feedbackList;
+
+    public Product() {
+    }
+
+    public Product(String productid) {
+        this.productid = productid;
+    }
+
+    public Product(String productid, String productname) {
+        this.productid = productid;
+        this.productname = productname;
+    }
+
+    public Product(String productid, String productname, Float productprice, Category categoryid, List<ProductDetails> productDetailsList, List<Feedback> feedbackList) {
+        this.productid = productid;
+        this.productname = productname;
+        this.productprice = productprice;
+        this.categoryid = categoryid;
+        this.productDetailsList = productDetailsList;
+        this.feedbackList = feedbackList;
+    }
+
+    public String getProductid() {
+        return productid;
+    }
+
+    public void setProductid(String productid) {
+        this.productid = productid;
+    }
+
+    public String getProductname() {
+        return productname;
+    }
+
+    public void setProductname(String productname) {
+        this.productname = productname;
+    }
+
+    public Float getProductprice() {
+        return productprice;
+    }
+
+    public void setProductprice(Float productprice) {
+        this.productprice = productprice;
+    }
+
+    public Category getCategoryid() {
+        return categoryid;
+    }
+
+    public void setCategoryid(Category categoryid) {
+        this.categoryid = categoryid;
+    }
+
+    @XmlTransient
+    public List<ProductDetails> getProductDetailsList() {
+        return productDetailsList;
+    }
+
+    public void setProductDetailsList(List<ProductDetails> productDetailsList) {
+        this.productDetailsList = productDetailsList;
+    }
+
+    @XmlTransient
+    public List<Feedback> getFeedbackList() {
+        return feedbackList;
+    }
+
+    public void setFeedbackList(List<Feedback> feedbackList) {
+        this.feedbackList = feedbackList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (productid != null ? productid.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Product)) {
+            return false;
+        }
+        Product other = (Product) object;
+        if ((this.productid == null && other.productid != null) || (this.productid != null && !this.productid.equals(other.productid))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Product[ productid=" + productid + " ]";
+    }
+
+}
---
+++ src/java/entity/ProductDetails.java
@@ -0,0 +1,149 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author melvi
+ */
+@Entity
+@Table(name = "PRODUCT_DETAILS")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "ProductDetails.findAll", query = "SELECT p FROM ProductDetails p")
+    , @NamedQuery(name = "ProductDetails.findByProductdetailsid", query = "SELECT p FROM ProductDetails p WHERE p.productdetailsid = :productdetailsid")
+    , @NamedQuery(name = "ProductDetails.findByProductsize", query = "SELECT p FROM ProductDetails p WHERE p.productsize = :productsize")
+    , @NamedQuery(name = "ProductDetails.findByQuantity", query = "SELECT p FROM ProductDetails p WHERE p.quantity = :quantity")})
+public class ProductDetails implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 15)
+    @Column(name = "PRODUCTDETAILSID")
+    private String productdetailsid;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 10)
+    @Column(name = "PRODUCTSIZE")
+    private String productsize;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "QUANTITY")
+    private int quantity;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "productDetails")
+    private List<OrderDetails> orderDetailsList;
+    @JoinColumn(name = "PRODUCTID", referencedColumnName = "PRODUCTID")
+    @ManyToOne(optional = false)
+    private Product productid;
+
+    public ProductDetails() {
+    }
+
+    public ProductDetails(String productdetailsid) {
+        this.productdetailsid = productdetailsid;
+    }
+
+    public ProductDetails(String productdetailsid, String productsize, int quantity) {
+        this.productdetailsid = productdetailsid;
+        this.productsize = productsize;
+        this.quantity = quantity;
+    }
+
+    public ProductDetails(String productdetailsid, String productsize, int quantity, List<OrderDetails> orderDetailsList, Product productid) {
+        this.productdetailsid = productdetailsid;
+        this.productsize = productsize;
+        this.quantity = quantity;
+        this.orderDetailsList = orderDetailsList;
+        this.productid = productid;
+    }
+
+    public String getProductdetailsid() {
+        return productdetailsid;
+    }
+
+    public void setProductdetailsid(String productdetailsid) {
+        this.productdetailsid = productdetailsid;
+    }
+
+    public String getProductsize() {
+        return productsize;
+    }
+
+    public void setProductsize(String productsize) {
+        this.productsize = productsize;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @XmlTransient
+    public List<OrderDetails> getOrderDetailsList() {
+        return orderDetailsList;
+    }
+
+    public void setOrderDetailsList(List<OrderDetails> orderDetailsList) {
+        this.orderDetailsList = orderDetailsList;
+    }
+
+    public Product getProductid() {
+        return productid;
+    }
+
+    public void setProductid(Product productid) {
+        this.productid = productid;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (productdetailsid != null ? productdetailsid.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ProductDetails)) {
+            return false;
+        }
+        ProductDetails other = (ProductDetails) object;
+        if ((this.productdetailsid == null && other.productdetailsid != null) || (this.productdetailsid != null && !this.productdetailsid.equals(other.productdetailsid))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.ProductDetails[ productdetailsid=" + productdetailsid + " ]";
+    }
+
+}
